244d6c7a041d782bd5ea2b34a261e404d576ac0f
erinkelsey <erinszilagyi@gmail.com>
2020-10-08 20:49:15 -0600

    Add dynamic notes to keeper app

--- keeper-app/src/components/App.js
+++ keeper-app/src/components/App.js
@@ -4,10 +4,16 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 
+import notes from "../notes";
+
+const createNotes = (noteItem) => (
+  <Note title={noteItem.title} content={noteItem.content} />
+);
+
 const App = () => (
   <div>
     <Header />
-    <Note />
+    {notes.map(createNotes)}
     <Footer />
   </div>
 );
