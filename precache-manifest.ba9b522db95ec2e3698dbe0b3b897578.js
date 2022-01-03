bb943f7114838f9455814983dcfe07d001210bb1
erinkelsey <erinszilagyi@gmail.com>
2022-01-03 17:11:12 -0500

    Updates

---
+++ precache-manifest.ba9b522db95ec2e3698dbe0b3b897578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185ec704e215407bfe6b4ef0627c4869",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8975e09a5d719387262e",
+    "url": "/monsters-rolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8975e09a5d719387262e",
+    "url": "/monsters-rolodex/static/js/main.16b897d1.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
